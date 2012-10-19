db476ff68282d0ed881c319096314d87f9037ff9
Elise Huard <github@elisehuard.be>
2012-10-19 16:22:22 +0100

    experimenting with single value decomposition

--- digit-recognizer.R
+++ digit-recognizer.R
@@ -7,17 +7,37 @@ train <- read.csv("data/train.csv")
 train.data <- train[0:dim(train)[1],2:dim(train)[2]]
 train.label <- train[0:dim(train)[1],1]
 
-library(pixmap)
-library(ggplot2)
+# plotting
 imageData <- matrix(train.data[1,],28,28)
-bitmap <- pixmap(imageData)
-ggplot(bitmap)
+imageData <- do.call(rbind, lapply(imageData, unlist))
+# makes it back into one long list, so convert to matrix again?
+imageData <- matrix(imageData, 28, 28)
+image(imageData)
 
+# subsets per label, for analysis
+train_1 <- subset(train, label == 1)
+
+# test data to get an output from
 test.data <- read.csv("data/test.csv")
 
 # neural net
 # challenge: 785 neurons in hidden layer too much to calculate
+# response: svd
 library(nnet)
+compress_image <- function(row) {
+  imageData <- matrix(row,28,28)
+  imageData <- do.call(rbind, lapply(imageData, unlist))
+  # makes it back into one long list, so convert to matrix again?
+  imageData <- matrix(imageData, 28, 28)
+  imageData.svd <- svd(imageData)
+  u <- imageData.svd$u
+  d <- imageData.svd$d
+  v <- imageData.svd$v
+  i <- 10
+  imageData.compressed <- u[,1:i] %*% diag(d[1:i]) %*% t(v[,1:i])
+  imageData.compressed <- matrix(imageData.compressed,1,784)
+}
+train.data.compressed <- apply(train.data, 1, compress_image)
 model <- nnet(train.data, train.label, size = 785)
 
 library(e1071)
